9a9e59800e522f5802723034cc55f3199251009d
fmichalski03 <filip.michalski.1@student.put.poznan.pl>
2026-08-29 15:50:53 +0200

    add spine torque penalty metrics to reward evaluation

--- rl_x/environments/custom_mujoco/robot_locomotion/mujoco/reward_functions/simplified_landing_reward.py
+++ rl_x/environments/custom_mujoco/robot_locomotion/mujoco/reward_functions/simplified_landing_reward.py
@@ -340,6 +340,20 @@ class SimplifiedLandingReward:
         )
 
         torque_reward = self.joint_torque_coeff * -np.mean(np.square(tau))
+        if getattr(self.env, "spine_locked", False):
+            leg_tau = tau
+            spine_tau = 0.0
+        else:
+            spine_tau = tau[0]
+            leg_tau = tau[1:]
+
+        info = self.env.internal_state["info"]
+        info["metrics/leg_torque_penalty"] = self.joint_torque_coeff * -np.mean(np.square(leg_tau))
+        
+        if getattr(self.env, "spine_locked", False):
+            info["metrics/spine_torque_penalty"] = 0.0
+        else:
+            info["metrics/spine_torque_penalty"] = self.joint_torque_coeff * -np.square(spine_tau)
         action_rate_reward = self.action_rate_coeff * -np.mean(np.square(action - self.env.internal_state["last_action"]))
 
         # =====================================================================
